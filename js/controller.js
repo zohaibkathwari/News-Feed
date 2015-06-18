508f6d159187b17e8c4d0ccdd93320ae1a798734
zuhaib kathwari <zohaib.kathwari@gmail.com>
2015-06-18 13:16:38 +0530

    implemented responsive iframe, some minor CSS change, modified index.html

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,16 @@
 /*jslint vars: true, plusplus: true, browser: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define */
 /*global $, jQuery, DataFactory, angular, newsFeed*/
-newsFeed.controller("FeedsController", ["$scope", "$http", "DataFactory",
-    function ($scope, $http, DataFactory) {
+newsFeed.controller("FeedsController", ["$scope", "$sce", "DataFactory",
+        function ($scope, $sce, DataFactory) {
         "use strict";
         $scope.selected = null;
         $scope.setSelected = function (item) {
             $scope.selected = item;
+            $scope.iframeURL = $scope.selected.guid_asArray[0].__text;
+            $scope.trustedURL = $sce.trustAsResourceUrl($scope.iframeURL);
+            console.log('iframeURL:', $scope.iframeURL);
+            console.log('trustedURL:', $scope.trustedURL);
         };
         var callback = function (jsonData) {
             console.log('JSON DATA:', jsonData);
